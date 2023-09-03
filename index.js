f3974e4665a88a70c1921d021bff8e4853cfd478
Fernando Hernández <f_hernandez_e@hotmail.com>
2023-09-02 23:21:34 -0400

    Agrega configuración CORS para permitir solicitudes desde cualquier origen

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ require('dotenv').config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('tiny'));
+app.use(cors());
 
 app.use('/', viewRoutes);
 app.use(routeVersion, userRoutes);
